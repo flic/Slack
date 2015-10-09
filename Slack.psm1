287cd42abe42958e8af7d737dd2cb280d9bbf51b
Fredrik Furtenbach <fredrik@fredriksmacbook.invmgt.wan>
2015-10-09 08:37:37 +0200

    Formatting changes

--- Slack.psm1
+++ Slack.psm1
@@ -1,20 +1,37 @@
-﻿Function New-SlackbotMessage
+﻿
+Function Send-SlackAlert
 {
-    param ([string]$URL,[string]$Token,[string]$Channel,[string]$Message)
+    param ([string]$Webhook,[string]$Channel="",[string]$SourceHost="",[string]$Source="",[string]$Severity="",[string]$Message,[string]$User="Powershell",[string]$IconURL="",[string]$Color="danger")
 
-    if (($URL -eq "") -or ($Token -eq "") -or ($Channel -eq "") -or ($Message -eq "")) {
-        Write-Host "Usage: New-SlackbotMessage -URL ""<team url, without slack.com>"" -Token ""<your slackbot token-id>"" -Channel ""slack channel name (without #)"" -Message ""<message>"""
+    if (($Webhook -eq "") -or ($Message -eq "")) {
+        Write-Host "Usage: Send-Slackalert -Webhook ""<Webhook service ID>"" -Channel ""slack channel name (without #)"" -SourceHost ""<hostname>"" -Source ""<source name>"" -Severity ""<severity text>"" -User ""<user name>"" -IconURL ""<URL to png>"" -Color ""<name or RGB>"" -Message ""<message>"""
         break
     }
-
-    if ($URL -like "*.slack.com") {
-        $URL = $URL.Substring(0,$URL.IndexOf("."))
-    }    
 
     if ($Channel.StartsWith("#")) {
         $Channel=$Channel.TrimStart(1)
     }
-    
-    Invoke-WebRequest -uri "https://$URL.slack.com/services/hooks/slackbot?token=$Token&channel=%23$Channel" -Method POST -Body "$Message"
-
+    if ($Webhook.StartsWith("/")) {
+        $Channel=$Channel.TrimStart(1)
+    }
+  
+    $payload = @{channel="#$Channel";username="$User";icon_url="$IconURL";
+        attachments=@(
+            @{
+                fallback="[$SourceHost][$Severity]";
+                pretext="*Source: $Source*";
+                text="``````$Message``````";
+                color="$Color";
+                mrkdwn_in=@("pretext","text");
+                fields=@(
+                    @{title="Host";value="$SourceHost";short=$true},
+                    @{title="Severity";value="$Severity";short=$true}
+                )
+            }
+        )
+   }
+      
+    $payload =  [System.Text.Encoding]::UTF8.GetBytes($(convertTo-JSON -depth 6 $payload))
+ 
+    Invoke-RestMethod -Uri "https://hooks.slack.com/services/$Webhook" -Method Post -Body ($payload)
 }
