dcc0f6ac5313af490b32850f942356c1a0998def
Fredrik Furtenbach <fredrik@fredriksmacbook.invmgt.wan>
2015-11-20 08:32:34 +0100

    Added Send-SlackMessage

--- Slack.psm1
+++ Slack.psm1
@@ -1,7 +1,17 @@
 ﻿
 Function Send-SlackAlert
 {
-    param ([string]$Webhook,[string]$Channel="",[string]$SourceHost="",[string]$Source="",[string]$Severity="",[string]$Message,[string]$User="Powershell",[string]$IconURL="",[string]$Color="danger")
+    param (
+        [string]$Webhook,
+        [string]$Channel="",
+        [string]$SourceHost="",
+        [string]$Source="",
+        [string]$Severity="",
+        [string]$Message,
+        [string]$User="Powershell",
+        [string]$IconURL="",
+        [string]$Color="danger"
+    )
 
     if (($Webhook -eq "") -or ($Message -eq "")) {
         Write-Host "Usage: Send-Slackalert -Webhook ""<Webhook service ID>"" -Channel ""slack channel name (without #)"" -SourceHost ""<hostname>"" -Source ""<source name>"" -Severity ""<severity text>"" -User ""<user name>"" -IconURL ""<URL to png>"" -Color ""<name or RGB>"" -Message ""<message>"""
@@ -35,3 +45,34 @@ Function Send-SlackAlert
  
     Invoke-RestMethod -Uri "https://hooks.slack.com/services/$Webhook" -Method Post -Body ($payload)
 }
+
+Function Send-SlackMessage
+{
+    param (
+        [string]$Webhook,
+        [string]$Channel="",
+        [string]$Message,
+        [string]$User="Powershell",
+        [string]$IconURL=""
+    )
+
+    if (($Webhook -eq "") -or ($Message -eq "")) {
+        Write-Host "Usage: Send-SlackMessage -Webhook ""<Webhook service ID>"" -Channel ""slack channel name (without #)"" -User ""<user name>"" -IconURL ""<URL to png>"" -Message ""<message>"""
+        break
+    }
+
+    if ($Channel.StartsWith("#")) {
+        $Channel=$Channel.TrimStart(1)
+    }
+    if ($Webhook.StartsWith("/")) {
+        $Channel=$Channel.TrimStart(1)
+    }
+  
+    $payload = @{channel="#$Channel";username="$User";icon_url="$IconURL";text="$Message"}      
+    $payload =  [System.Text.Encoding]::UTF8.GetBytes($(convertTo-JSON -depth 6 $payload))
+ 
+    Invoke-RestMethod -Uri "https://hooks.slack.com/services/$Webhook" -Method Post -Body ($payload)
+}
+
+Export-ModuleMember -Function Send-SlackAlert
+Export-ModuleMember -Function Send-SlackMessage
